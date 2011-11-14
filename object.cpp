ae943c61d53de0c10471f37a8ee1c658cda8510c
'Alexey Khudyakov <alexey skladnoy {at} gmail com>
2011-11-14 11:11:31 +0300

    Partially implement drawing primitives

--- object.cpp
+++ object.cpp
@@ -1,8 +1,117 @@
 
 #include "object.hpp"
 
-void PlotObject::setLineColor()
-{}
+#include <algorithm>
 
-void PlotObject::setLineWidth(int)
-{}
+#include <TH1.h>
+#include <TGraph.h>
+#include <TCanvas.h>
+
+
+
+// ================================================================ //
+// ==== Plot
+
+void Plot::clear() {
+    canvas->Clear();
+    objStack.resize(0);
+}
+
+void Plot::draw() {
+    canvas->Clear();
+}
+
+void Plot::redraw() {
+}
+
+
+
+// ================================================================ //
+// ==== Histogram
+
+void PlotHist::plotOn(Plot*) {
+    hist->Draw( "SAME" );
+}
+
+void PlotHist::setLineWidth(int width) {
+    hist->SetLineWidth(width);
+}
+
+void PlotHist::setLineColor(Plot::Color col) {
+    hist->SetLineColor(col);
+}
+
+RangeM PlotHist::xRange() const {
+    // FIXME!
+    return boost::optional<Range>();
+}
+
+RangeM PlotHist::yRange() const {
+    // FIXME!
+    return boost::optional<Range>();
+}
+
+
+
+// ================================================================ //
+// ==== Graph
+
+void PlotGraph::plotOn(Plot*) {
+    graph->Draw( "SAME" );
+}
+
+void PlotGraph::setLineWidth(int width) {
+    graph->SetLineWidth(width);
+}
+
+void PlotGraph::setLineColor(Plot::Color col) {
+    graph->SetLineColor(col);
+}
+
+RangeM PlotGraph::xRange() const {
+    int n = graph->GetN();
+    if( n == 0 )
+        return boost::optional<Range>();
+    
+    double* xs = graph->GetX();
+    double hi = *std::max_element(xs, xs+n);
+    double lo = *std::min_element(xs, xs+n);
+    return boost::optional<Range>( Range(hi,lo) );
+}
+
+RangeM PlotGraph::yRange() const {
+    int n = graph->GetN();
+    if( n == 0 )
+        return boost::optional<Range>();
+    
+    double* ys = graph->GetY();
+    double hi = *std::max_element(ys, ys+n);
+    double lo = *std::min_element(ys, ys+n);
+    return boost::optional<Range>( Range(hi,lo) );
+}
+
+
+
+// ================================================================ //
+// ==== Line
+
+
+void PlotLine::plotOn(Plot* cxt) {
+    // FIXME
+}
+
+void PlotLine::setLineWidth(int w) {
+    width = w;
+}
+
+void PlotLine::setLineColor(Plot::Color col) {
+    color = col;
+}
+
+RangeM PlotLine::xRange() const {
+    return boost::optional<Range>();
+}
+
+RangeM PlotLine::yRange() const {
+    return boost::optional<Range>();
+}
--- object.hpp
+++ object.hpp
@@ -4,8 +4,14 @@
 #include <vector>
 #include <boost/optional.hpp>
 #include <boost/shared_ptr.hpp>
+#include <boost/scoped_ptr.hpp>
 #include <boost/noncopyable.hpp>
 
+#define UNUSED(x) do{ (void)(x); }while(0)
+
+// ================================================================ //
+// == Small types
+// ================================================================ //
 
 // Axix range
 struct Range {
@@ -16,13 +22,48 @@ struct Range {
     double hi;
 };
 
+typedef boost::optional<Range> RangeM;
+
+// ================================================================ //
+// == Plot types
+// ================================================================ //
 
 class PlotObject;
+
+class TCanvas;
 class TLegend;
+class TGraph;
+class TH1;
 
 class Plot : public boost::noncopyable {
 public:
+    enum Color {
+        BLACK   = 1,
+        RED     = 2,
+        GREEN   = 3,
+        BLUE    = 4,
+        YELLOW  = 5,
+        MAGENTA = 6,
+        CYAN    = 7,
+        FOREST  = 8,
+        VIOLET  = 9
+    };
+    enum Line {
+        Vertical,
+        Horizontal
+    };
+
+    // Clear plot and delete entire object stack
+    void clear();
+    
+    // Draw everything
+    void draw();
+    // Redraw. Just makes canvas update
+    void redraw();
+
+    // TCanvas* getCanvas() { return cnv; }
 private:
+    TCanvas*                                     canvas;
     std::vector< boost::shared_ptr<PlotObject> > objStack; // Stack of objects
     boost::optional<Range>                       xRange;   // X range
     boost::optional<Range>                       yRange;   // Y range
@@ -32,33 +73,72 @@ private:
 // Object which
 class PlotObject {
 public:
+    virtual ~PlotObject() {}
+    
     // Add object on a plot
     virtual void plotOn(Plot* cxt) = 0;
 
     // X range for object
-    virtual boost::optional<Range> xRange() const = 0;
+    virtual RangeM xRange() const = 0;
     // Y range for object
-    virtual boost::optional<Range> yRange() const = 0;
+    virtual RangeM yRange() const = 0;
 
     // Set color of line
-    virtual void setLineColor();
+    virtual void setLineColor(Plot::Color) {}
     // Set width of line
-    virtual void setLineWidth(int w);
+    virtual void setLineWidth(int width)   {UNUSED(width);}
 };
 
 
 
 
 class PlotHist : public PlotObject {
+    // Create graph out of ROOT graph. Will take outnership of the
+    // copy.
+    PlotHist(TGraph* graph);
+
+    virtual void   plotOn(Plot* cxt);
+    virtual RangeM xRange() const;
+    virtual RangeM yRange() const;
+    virtual void   setLineWidth(int width);
+    virtual void   setLineColor(Plot::Color);
+private:
+    boost::scoped_ptr<TH1> hist;
 };
 
 class PlotGraph : public PlotObject {
+public:
+    // Create graph out of ROOT graph. Will take outnership of the
+    // copy.
+    PlotGraph(TGraph* graph);
+
+    virtual void   plotOn(Plot* cxt);
+    virtual RangeM xRange() const;
+    virtual RangeM yRange() const;
+    virtual void   setLineWidth(int width);
+    virtual void   setLineColor(Plot::Color);
+private:
+    boost::scoped_ptr<TGraph> graph;
 };
 
-class VLine : public PlotObject {
-};
 
-class HLine : public PlotObject {
+// Line of the plot. Ranges are determined automatically
+class PlotLine : public PlotObject {
+public:
+    // Create vertical line at position x
+    PlotLine(Plot::Line orientation, double x);
+
+    virtual ~PlotLine() {}
+    virtual void   plotOn(Plot* cxt);
+    virtual RangeM xRange() const;
+    virtual RangeM yRange() const;
+    virtual void   setLineWidth(int width);
+    virtual void   setLineColor(Plot::Color);
+private:
+    Plot::Line orientation;
+    double     x;
+    int        color;
+    int        width;
 };
 
 #endif /* ROOT_PLOT__HPP__ */
