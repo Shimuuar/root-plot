cd331218b144195dcbb4a4d4810af90fca5a65d3
Aleksey Khudyakov <alexey.skladnoy@gmail.com>
2011-11-29 17:25:03 +0300

    Sane ROOT

--- main.cpp
+++ main.cpp
@@ -1,11 +1,25 @@
 
 #include "RtPlot.hpp"
 
+#include <signal.h>
+
+#include <TH1.h>
+#include <TROOT.h>
+#include <TStyle.h>
+
 
 
 int main()
 // int main(int argc, char** argv)
 {
+    // Uninstall SIGSEVG handler
+    signal( SIGSEGV, SIG_DFL );
+    // Do not histograms to current dir
+    TH1::AddDirectory(kFALSE);
+    // Reasonable color schemes
+    gROOT ->SetStyle("Plain");
+    gStyle->SetPalette(1);
+
     RtPlot app;
     app.Run();
     return 0;
