0205d3afac73db3774553e342f12ec9d3b7d4c9c
Aleksey Khudyakov <alexey.skladnoy@gmail.com>
2012-07-17 20:41:48 +0400

    Add more extra combinators

--- hep-root-plot/HEP/ROOT/Plot/Extra.hs
+++ hep-root-plot/HEP/ROOT/Plot/Extra.hs
@@ -3,6 +3,8 @@
 module HEP.ROOT.Plot.Extra (
     rectangle
   , irectangle
+  , lineRect
+  , ilineRect
   ) where
 
 import HEP.ROOT.Plot
@@ -26,3 +28,25 @@ irectangle :: (Int,Int)         -- ^ X coordinates
 irectangle (x1,x2) (y1,y2)
   = rectangle (fromIntegral x1 - 0.5 :: Double, fromIntegral x2 + 0.5)
               (fromIntegral y1 - 0.5 :: Double, fromIntegral y2 + 0.5)
+
+
+-- | Draw line rectangle using polygon command
+lineRect :: Real a
+         => (a,a)              -- ^ X coordinates
+         -> (a,a)              -- ^ Y coordinates
+         -> Cmd Command
+lineRect (realToFrac -> x1, realToFrac -> x2) (realToFrac -> y1, realToFrac -> y2) = do
+  add $ Graph [ (x1,y1)
+              , (x1,y2)
+              , (x2,y2)
+              , (x2,y1)
+              , (x1,y1)
+              ]
+
+-- | Rectangle for integer plots. It's padded with 0.5 on every side
+ilineRect :: (Int,Int)         -- ^ X coordinates
+          -> (Int,Int)         -- ^ Y coordinates
+          -> Cmd Command
+ilineRect (x1,x2) (y1,y2)
+  = lineRect (fromIntegral x1 - 0.5 :: Double, fromIntegral x2 + 0.5)
+             (fromIntegral y1 - 0.5 :: Double, fromIntegral y2 + 0.5)
