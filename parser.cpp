5d6355c316f0a6a025be9dd7a7f75a78647e99e6
Aleksey Khudyakov <alexey.skladnoy@gmail.com>
2012-10-21 20:31:55 +0400

    Use new method for determining where data is located

--- parser.cpp
+++ parser.cpp
@@ -29,6 +29,7 @@ public:
 // Accumulator for graphs
 class AccumGraph : public LineAccum {
 public:
+    AccumGraph();
     virtual ~AccumGraph();
     virtual bool flush(Plot*);
     virtual bool feedLine(const std::string& str);
@@ -36,15 +37,74 @@ protected:
     typedef std::vector<double> Column;
     typedef std::vector<Column> ColList;
 
+    // Data columns
     ColList cols;
-
+    Column  surrogateX;
     size_t  colSize()     { return cols[0].size();  }
-    double* colPtr(int i) { return &( cols[i][0] ); }
+
+    // Indices for columns
+    int i_x;      // X
+    int i_dx;     // Symmetric error for X
+    int i_udx;    // Upper error for X
+    int i_ldx;    // Lower error for X
+    int i_y;      // Y
+    int i_dy;     // Symmetric error for Y
+    int i_udy;    // Upper error for Y
+    int i_ldy;    // Lower error for Y
+
+    bool noHeader();   // Ho header is given
+    bool unusableData(); // Some indices are out of range
+    // Getter for columns
+    double* getX();
+    double* getDX()  { return getCol( i_dx );  }
+    double* getUDX() { return getCol( i_udx ); }
+    double* getLDX() { return getCol( i_ldx ); }
+    double* getY()   { return getCol( i_y );   }
+    double* getDY()  { return getCol( i_dy );  }
+    double* getUDY() { return getCol( i_udy ); }
+    double* getLDY() { return getCol( i_ldy ); }
+    double* getCol(int i);
 };
+
+AccumGraph::AccumGraph() :
+    i_x(-1), i_dx(-1), i_udx(-1), i_ldx(-1),
+    i_y(-1), i_dy(-1), i_udy(-1), i_ldy(-1)
+{}
 
 AccumGraph::~AccumGraph()
 {}
 
+bool AccumGraph::noHeader() {
+    return i_x < 0 && i_dx < 0 && i_udx < 0 && i_ldx < 0 &&
+           i_y < 0 && i_dy < 0 && i_udy < 0 && i_ldy < 0;
+}
+
+bool AccumGraph::unusableData() {
+    int n = cols.size();
+    return i_x >= n || i_dx >= n || i_udx >= n || i_ldx >= n ||
+           i_y >= n || i_dy >= n || i_udy >= n || i_ldy >= n ||
+           n == 0;
+}
+
+double* AccumGraph::getX() {
+    double* x = getCol( i_x );
+    if( x == 0 ) {
+        size_t n = colSize();
+        surrogateX.resize( n );
+        for(unsigned i = 0; i < n; i++)
+            surrogateX[i] = i;
+        x = &surrogateX[0];
+    }
+    return x;
+}
+
+double* AccumGraph::getCol(int i) {
+    int n = cols.size();
+    if( i < 0 || i >= n )
+        return 0;
+    else
+        return &(cols[i][0]);
+}
 
 bool AccumGraph::feedLine(const std::string& str) {
     // Lexing buffer line
@@ -62,6 +122,7 @@ bool AccumGraph::feedLine(const std::string& str) {
             s[i] = '0';
         } else if (wordEnded) {
             offs.push_back( i );
+            wordEnded = false;
         }
     }
     // Resize columns array if needed
@@ -82,40 +143,50 @@ bool AccumGraph::feedLine(const std::string& str) {
 }
 
 bool AccumGraph::flush(Plot* plot) {
-    switch ( cols.size() ) {
-    case 1: {
-        size_t n = colSize();
-        std::vector<double> xs;
-        xs.resize( n );
-        for( unsigned i = 0; i < n; i++ )
-            xs[i] = i;
-        plot->pushObject(
-            boost::make_shared<PlotGraph>(
-                new TGraph( n, &(xs[0]), colPtr(0) ) ) );
-        return true;
+    // Use default column number if none have been given
+    if( noHeader() ) {
+        switch( cols.size() ) {
+        case 1:
+            i_y  = 0;
+            break;
+        case 2:
+            i_x  = 0;
+            i_y  = 1;
+            break;
+        case 3:
+            i_x  = 0;
+            i_y  = 1;
+            i_dy = 2;
+            break;
+        }
     }
-    case 2:
-        plot->pushObject(
-            boost::make_shared<PlotGraph>(
-                new TGraph( colSize(), colPtr(0), colPtr(1) ) ) );
-        return true;
-    case 3:
-        plot->pushObject(
-            boost::make_shared<PlotGraph>(
-                new TGraphErrors( colSize(), colPtr(0), colPtr(1), 0, colPtr(2)) ) );
-        return true;
-    case 4:
-        plot->pushObject(
-            boost::make_shared<PlotGraph>(
-                new TGraphErrors( colSize(), colPtr(0), colPtr(1), colPtr(2), colPtr(3)) ) );
-        return true;
-    default:
+    // Check that indices are OK
+    if( unusableData() )
         return false;
+    // Get columns
+    double *x  = getX();
+    double *y  = getY();
+    double *dx = getDX();
+    double *dy = getDY();
+    // Check mandatory columns
+    if( !x || !y )
+        return false;
+    // We have error bars
+    if( dx || dy ) {
+        plot->pushObject(
+            boost::make_shared<PlotGraph>(
+                new TGraphErrors( colSize(), x, y, dx, dy ) ) );
+        return true;
     }
+    // Plain old graphs
+    plot->pushObject(
+        boost::make_shared<PlotGraph>(
+            new TGraph( colSize(), x, y ) ) );
+    return true;
 }
 
 
-// ================================================================    
+// ================================================================
 // Accumulator for barchart
 class AccumBarchart : public AccumGraph {
 public:
@@ -127,18 +198,32 @@ AccumBarchart::~AccumBarchart()
 {}
 
 bool AccumBarchart::flush(Plot* plot) {
-    switch( cols.size() ) {
-    case 2:
+    // Use default column number if none have been given
+    if( noHeader() ) {
+        switch( cols.size() ) {
+        case 2:
+            i_x  = 0;
+            i_y  = 1;
+            break;
+        }
+    }
+    // Check that indices are OK
+    if( unusableData() )
+        return false;
+    // Get columns
+    double *x  = getX();
+    double *y  = getY();
+    if( x && y ) {
         plot->pushObject(
             boost::make_shared<PlotBarChart>(
-                new TGraph( colSize(), colPtr(0), colPtr(1) ) ) );
+                new TGraph( colSize(), x, y ) ) );
         return true;
-    default:
+    } else {
         return false;
     }
 }
 
-// ================================================================    
+// ================================================================
 // Accumulator for polygons
 class AccumPoly : public AccumGraph {
 public:
@@ -150,22 +235,20 @@ AccumPoly::~AccumPoly()
 {}
 
 bool AccumPoly::flush(Plot* plot) {
-    switch ( cols.size() ) {
-    case 2: {
-        if( colSize() == 0 )
-            return false;
-        std::vector<double>& xs = cols[0];
-        std::vector<double>& ys = cols[1];
-        xs.push_back( xs[0] );
-        ys.push_back( ys[0] );
-        plot->pushObject(
-            boost::make_shared<PlotPoly>(
-                new TPolyLine( xs.size(), &(xs[0]), &(ys[0])) ) );
-        return true;
-    }
-    default:
+    int n = cols.size();
+    if( i_x < 0 || i_x >= n ||
+        i_y < 0 || i_y >= n || colSize() < 3)
+    {
         return false;
     }
+    std::vector<double>& xs = cols[0];
+    std::vector<double>& ys = cols[1];
+    xs.push_back( xs[0] );
+    ys.push_back( ys[0] );
+    plot->pushObject(
+        boost::make_shared<PlotPoly>(
+            new TPolyLine( xs.size(), &(xs[0]), &(ys[0])) ) );
+    return true;
 }
 
 } // namespace
